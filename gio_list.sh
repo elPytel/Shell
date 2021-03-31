30f88acdee3751dc0638b22139d2232691aef234
elPytel <jaroslav.korner1@gmail.com>
2021-03-31 20:56:47 +0200

    Přidal jsem script pro lepši vypisování názvů souborů na google disku

--- gio_list.sh
+++ gio_list.sh
@@ -13,7 +13,22 @@ $DEBUG && echo "path: $BASEDIR"
 # colors
 source $BASEDIR/colors.sh
 
-gio list -a "standard::display-name" | cut -d"=" -f2
+#gio list -a "standard::display-name"
+#gio list -a "standard::display-name" | cut -f3,4 | tr -s ":" | tr -s [:space:] | tr "\t" ":"
+#exit
+
+text=$(gio list -a "standard::display-name" | cut -f1,3,4 | tr -s ":" | tr -s [:space:] | tr "\t" ":" | tr " " "_")
+for line in $text; do
+	$DEBUG && echo $line
+	printf "%.5s " "$(echo $line | cut -d":" -f1)"
+	format=$(echo $line | cut -d":" -f2)
+	file_name=$(echo $line | cut -d":" -f4 | cut -d"=" -f2)
+	case $format in
+		"(regular)") echo "$file_name";;
+		"(directory)") echo -e "${Blue}$file_name${NC}";;
+		*) echo "$file_name";;
+	esac
+done
 
 exit 0
 #END
