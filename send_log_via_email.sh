924d378f8ccfe12d3f8919f7b5f20b56943e84de
elPytel <jaroslav.korner1@gmail.com>
2021-04-18 01:04:19 +0200

    email edit

--- send_log_via_email.sh
+++ send_log_via_email.sh
@@ -19,24 +19,28 @@ path=$(dirname "$0")         # adresa k tomuto skriptu
 source $path/tools/colors.sh
 
 config=".email.conf"
-subject="Logs"  # datum a nazev
+subject="Logs $(date | cut -d"," -f1)"			# datum a nazev
 $DEBUG && echo "Subject: $subject"
 
 # odeslani e-mailem
-FILES="~/.my.log ~/.backup.log ~/.startup.log"
+FILES=$(cat $path/$config | grep "files2send=" | cut -d"=" -f2)
 for FILE in $FILES; do
-	files="$files -A $FILE"
+	files="$files -A "$(eval echo $FILE)
+	#files="$files -A $FILE"
 done
+#files=~/.my.log
 $DEBUG && echo "Files: $files"
 
+
 # odeslani
-email=$(cat $path/$config)
+email=$(cat $path/$config | grep "e-mail=" | cut -d"=" -f2)
 echo -e "${Green}Sending to email:${NC} $email"
-#mail -s $subject -A $subject $email < /dev/null; ec=$?
-ec=0
+#mail -s "$subject" -A ~/.my.log -A ~/.backup.log -A ~/.startup.log $email < /dev/null; ec=$?
+mail -s "$subject" $files $email < /dev/null; ec=$?
+#ec=0
 case $ec in
-        0) echo "E-mail: $subject, sent OK.";;
-        *) 
+	0) echo "E-mail: $subject, sent OK.";;
+    *) 
 		echo -e "${Red}ERROR:${NC} failed to sent!"
 		exit 1
 		;;
@@ -44,10 +48,17 @@ esac
 
 # prace se soubory
 for FILE in $FILES; do
+	#if [ $(echo $FILE | grep -c "~*") -gt 0 ]; then
+	#	name=$HOME/$(echo $FILE | tr -d  "~/")
+	#else
+	#	name=$FILE
+	#fi
+	
 	name=$FILE
-	name_OLD="$(echo $FILE | cut -d"." -f1,2)_OLD.log"
-	rm $name_OLD			# odstrani stary
-	mv $name $name_OLD; ec=$?	# prejmenuje novy na stary (_OLD)
+	$DEBUG && echo $name
+	name_OLD=$(echo $name | cut -d"." -f1,2)_OLD.log
+	eval rm $name_OLD				# odstrani stary
+	eval mv $name $name_OLD; ec=$?	# prejmenuje novy na stary (_OLD)
 	if [ $ec -eq 0 ]; then
 	       echo -e "File: $name renamed to: $name_OLD"
 	fi
