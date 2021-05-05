f2b2e51ea2cf1a38072e2bc6d5294db9c83eac33
elPytel <jaroslav.korner1@gmail.com>
2021-05-05 10:36:01 +0200

    sort conf

--- test/sort_em_all.sh
+++ test/sort_em_all.sh
@@ -3,8 +3,25 @@
 # Algoritmus pro trideni soubru podle klice do adresaru.
 # Jeho zaznam by mel mit cron pro spravnou funkci.
 
-#DEBUG=true
-DEBUG=false
+DEBUG=true
+#DEBUG=false
+
+BASEDIR=$(dirname "$0")         # adresa k tomuto skriptu
+#user=$(. "$BASEDIR"/tools/get_curent_user.sh)
+path="/home/$user/Shell"        # cesta k skriptum
+$DEBUG && echo "path: $BASEDIR"
+
+# colors
+#source "$BASEDIR"/tools/colors.sh
+
+config=".sort.conf"
+
+cat $config
+entry=$(grep -v "#" $config | grep ":" | cut -d":" -f1)
+toFolder=$(grep -v "#" $config | grep ":" | cut -d":" -f2)
+echo $(grep -v "#" $config | grep ":" )
+echo $entry
+echo $toFolder
 
 exit 0
 # END
