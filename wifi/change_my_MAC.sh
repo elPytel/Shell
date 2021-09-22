2bb7de70df4ea46d96cb2acc8ca1886c268c83e6
elPytel <jaroslav.korner1@gmail.com>
2021-09-23 01:47:38 +0200

    add list of MAC Vendors

--- wifi/change_my_MAC.sh
+++ wifi/change_my_MAC.sh
@@ -9,12 +9,73 @@ VERBOSE=false
 
 function printHelp () {
 	echo -e "USE:"
-	echo -e "  $(echo "$0" | tr "/" "\n" | tail -n 1) [interface] [MAC] COMMANDS"
+	echo -e "  $(echo "$0" | tr "/" "\n" | tail -n 1) -i [interface] -m [MAC] COMMANDS"
 	echo ""
 	echo -e "COMMANDS:"
 	echo -e "  -h --help \t print this text"
 	echo -e "  -d --debug\t enable debug output"
 	echo -e "  -v --verbose\t enable vebrose mode"
+	echo -e "  -l --list \t list all Vendors"
+	echo -e "  -f --file \t load file"
+	echo -e "  -i --interface set interface: <>"
+	echo -e "  -m --MAC  \t set MAC address: <>"
+	echo -e "  -v --vendor\tset Vendor: <>"
+	echo -e "  -R --random\tmake random pic of Vendor"
+}
+
+function listVendors () {
+	echo "Vendors: "
+	cat $file | grep "#" | tr " " "\n" | grep "#" -v
+}
+
+function setVendor () { #( vendor )
+	local _vendor="$1"
+	if [ $(echo "$(listVendors)" | grep $_vendor -c) -ne 1 ]; then
+		echo -e "${RED}ERROR: ${NC}unknown interface!" 1>&2
+                return 2
+	fi
+	vendor=$_vendor
+	return 0
+}
+
+function setFile () { #( file )
+	local _file=$1
+	if [ ! -f $_file ]; then
+		echo -e "${RED}ERROR: ${NC}file does not exist!" 1>&2
+		return 2
+	fi
+	file=$_file
+}
+
+function setInterface () { #( interface )
+        # not one argument
+	if [ $# -ne 1 ]; then
+		$VERBOSE && echo -e "ERROR: ${NC}interface not specified!"
+		return 1
+	fi
+	# empty argument
+	if [ -z "$1" ]; then
+		$VERBOSE && echo -e "ERROR: ${NC}empty interface!"
+		return 2
+	fi
+	interface=$1	# global!!
+	# not from existing interfaces
+	if [ $(echo "$interfaces" | grep -x "$interface" -c) -ne 1 ]; then
+		$VERBOSE && echo -e "ERROR: ${NC}invalid interface!"
+		return 3
+	fi
+	$DEBUG && echo -e "Interface set: $interface"
+        return 0
+}
+
+function setMAC () { #( MAC ) 
+	local mac
+	if [ $(echo "$mac" | grep -o . | grep ":" -c) -ne 5 ]; then
+		$VERBOSE && echo -e "ERROR: ${NC}invalid MAC!"
+		return 3
+	fi
+	MAC="$mac"
+	return 0
 }
 
 function listInterfaces () {
@@ -23,6 +84,8 @@ function listInterfaces () {
 
 interface=""
 interfaces="$(listInterfaces)"
+file="Vendors_MAC.txt"
+vendor=""
 
 if [ $(whoami) != "root" ]
 then
@@ -49,7 +112,13 @@ while [ $# -gt 0 ] ; do
 		-h | --help)    printHelp; exit 2;;
 		-d | --debug)   DEBUG=true;;
 		-v | --verbose) VERBOSE=true;;
-		*) interface="$1"; shift; MAC="$1"; $VERBOSE && echo "Interface set: $interface";;
+		-l | --list)	echo "$(listVendors)"; exit 3;;
+		-f | --file)	shift; setFile $1 || exit 4;;
+		-V | --vendor)	shift; setVendor $1 || exit 5;; 
+		-i | --interface) shift; setInterface $1 || exit 6;;
+		-m | --MAC)	shift; setMAC $1 || exit 7;;
+		-R | --random)	random=true;;
+		*) echo -e "Unknown parametr: $arg"; exit 1;;
 	esac
 
 	# next arg
