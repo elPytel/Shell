f0819f506f81d9743724d534842b3fc90101e99c
elPytel <jaroslav.korner1@gmail.com>
2022-02-14 21:17:29 +0100

    restrukturizace

--- tools/bashtest/test.sh
+++ tools/bashtest/test.sh
@@ -97,15 +97,6 @@ function test_file () { # ( file )
 	return 0
 }
 
-
-# prepinac verbose nastavi vsechny test funkce na vypisovani vsech vystupu, jinak vypisuje pouze chyby
-
-# bez argumentu najde vsechny soubory zacinajici test_ a koncisi .sh v pracovnim adresari
-# s argumetem spusti vybrany soubor
-
-# iteruje pre vsechny vybrane soubory a postupne spousti
-# najde v nich vsechny funkce oznacene function test_... () a prida je do seznamu funkci, ktere je potreba spustit
-
 # kazdou funkci spusti a odchyti jeji nermolni a chybovy vystup
 
 # chyby funkci se posilaji na chybovy vystup
@@ -115,11 +106,6 @@ function test_file () { # ( file )
 
 # kdyz funkce chybuje, tak vypisuji do konzole jeji kod
 # zvyrazneni radku, ktery navraci danou chybovou hodnotu
-
-# spocita uspesnot souboru, pocet proslych testu vs chybujicich
-
-
-#TODO assert alias
 
 passed=0
 faigled=0
